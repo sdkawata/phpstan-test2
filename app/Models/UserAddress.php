9f59c3ef22444bc71f9466ce4c76c044e8016894
sdkawata <kawata@sencorp.co.jp>
2023-12-21 19:24:34 +0900

    ofを追加

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-final class UserAddress
-{
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+final class UserAddress extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use App\Models\UserAddress;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -17,5 +18,9 @@ class ExampleTest extends TestCase
         $user = User::factory()->hasAddress()->create();
         $this->assertNotNull($user->email_verified_at);
         $this->assertNotNull($user->address);
+
+        $address = UserAddress::factory()->forUser()->create();
+        $this->assertNotNull($address->user);
+        $this->assertNotNull($address->user->email_verified_at);
     }
 }
