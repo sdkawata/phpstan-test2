9db625d413716aa09bf0a580b2463c5aa743538a
sdkawata <kawata@sencorp.co.jp>
2023-12-28 20:16:28 +0900

    routeの値を正しく推論できるか

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Models\User;
 use App\Models\UserAddress;
+use Illuminate\Http\Request;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -22,5 +23,7 @@ class ExampleTest extends TestCase
         $address = UserAddress::factory()->forUser()->create();
         $this->assertNotNull($address->user);
         $this->assertNotNull($address->user->email_verified_at);
+
+        $b = (new Request())->route()->parameter('a');
     }
 }
